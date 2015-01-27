ef65d2fb750bf54fce284de8b815d17c74227a8f
Nicolas Perriault <nperriault@gmail.com>
2015-01-27 16:37:38 +0100

    refs #3 - Added Chromium to Karma test matrix for CI.

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,9 @@
 module.exports = function(config) {
   "use strict";
-  config.set({
 
+  var cfg = {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: "",
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -18,19 +17,16 @@ module.exports = function(config) {
       }
     },
 
-
     // list of files / patterns to load in the browser
     files: [
       "src/js/app.js",
       "src/test/*_test.js"
     ],
 
-
     // list of files to exclude
     exclude: [
       "src/**/main.js"
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -44,37 +40,43 @@ module.exports = function(config) {
       transform: ["reactify", "6to5ify"]
     },
 
-
     // test results reporter to use
     // possible values: "dots", "progress"
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ["progress"],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ["Firefox"],
 
+    customLaunchers: {
+      "Chrome_travis_ci": {
+        base: "Chrome",
+        flags: ["--no-sandbox"]
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  });
+  };
+
+  if (process.env.TRAVIS) {
+    cfg.browsers.push("Chrome_travis_ci");
+  }
+
+  config.set(cfg);
 };
